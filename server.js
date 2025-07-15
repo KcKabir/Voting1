210728a05a7e07fc3499531274a4e03fa232d32f
KcKabir <KcKabir123@gmail.com>
2025-07-15 23:37:24 +0530

    more

--- server.js
+++ server.js
@@ -255,6 +255,15 @@ app.get("/logout", (req, res) => {
     res.redirect("/");
   });
 });
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  }
+});
+module.exports = pool;
+
 const port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log("IT WORKS!");
